cf55a062c2ecdfa3cf1bfa75713ef912cff4916f
Rendey <rendey28@hotmail.com>
2023-12-03 11:36:09 -0700

    Fix report and flakyness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario='random01';
+    const contrasena='random01';
+    
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(usuario,contrasena)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    cy.wait(10000)
   })
 })
