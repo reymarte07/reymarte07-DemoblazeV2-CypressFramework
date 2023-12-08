593eaaa855d76809db296d2d8ae1d1fd93e19104
Rendey <rendey28@hotmail.com>
2023-12-07 20:34:50 -0700

    crear usuario aleatorio

--- cypress/e2e/pages/common-page/common-page.methods.js
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -37,4 +37,18 @@ export class CommonPageMethods {
             expect(str).to.equal(expectedMessage)
           })
     }
+
+    //---------------- generate random user credentials -----------------
+    static generateRandomString(length=10) {
+        let result = '';
+        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          counter += 1;
+        }
+        return result;
+    }
+
 }
--- cypress/e2e/tests/registro-y-autenticacion.cy.js
+++ cypress/e2e/tests/registro-y-autenticacion.cy.js
@@ -3,6 +3,9 @@ import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { SignupMethods } from "../pages/signup/signup.methods";
 import { Logger } from "../util/logger";
 
+const user = CommonPageMethods.generateRandomString(); //generate random user
+const password = CommonPageMethods.generateRandomString(7); //generate random password
+
 describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
     it("Registro de usuario valido", ()=>{
         Logger.stepNumber(1)
@@ -15,8 +18,8 @@ describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
 
         Logger.stepNumber(3)
         Logger.step('Completar todos los campos obligatorios con información válida')
-        SignupMethods.insertUsername('sjkdfhjsdk')
-        SignupMethods.insertPassword('sjkdfhjsdk')
+        SignupMethods.insertUsername(user)
+        SignupMethods.insertPassword(password)
 
         Logger.stepNumber(4)
         Logger.step('Hacer clic en "Sign up" para registrar el usuario')
@@ -24,7 +27,5 @@ describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
         Logger.verification('Verificar que se muestre el mensaje "Sign up successful."')
         SignupMethods.VerifySignupSuccesfulMessageIsDisplayed();
     });
-
-    it 
 })
 
