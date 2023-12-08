a2a6a13526ccd3390c12370214c9edb06a3c0f3a
Rendey <rendey28@hotmail.com>
2023-12-07 18:10:57 -0700

    Agregar logger

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -1,21 +1,25 @@
+import { Logger } from "../../util/logger";
 import { LoginElements } from "./login.elements";
 
-export class LoginMethods{
-    static insertUsername(username){
-        LoginElements.textboxes.username.invoke('val', username)
-    }
+export class LoginMethods {
+  static insertUsername(username) {
+    LoginElements.textboxes.username.invoke("val", username);
+  }
 
-    static insertPassword(password){
-        LoginElements.textboxes.password.invoke('val', password)
-    }
+  static insertPassword(password) {
+    LoginElements.textboxes.password.invoke("val", password);
+  }
 
-    static clickOnLoginButton(){
-        LoginElements.buttons.login.click()
-    }
+  static clickOnLoginButton() {
+    LoginElements.buttons.login.click();
+  }
 
-    static login(username,password){
-        this.insertUsername(username)
-        this.insertPassword(password)
-        this.clickOnLoginButton()
-    }   
+  static login(username, password) {
+    Logger.subStep("Insert username");
+    this.insertUsername(username);
+    Logger.subStep("Insert password");
+    this.insertPassword(password);
+    Logger.subStep("Click on Login button");
+    this.clickOnLoginButton();
+  }
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,23 @@
 import { LoginMethods } from "./pages/login/login.methods";
+import { Logger } from "./util/logger";
 
-describe('template spec', () => {
-  it('passes', () => {
-    const usuario='random01';
-    const contrasena='random01';
-    
-    cy.visit('https://www.demoblaze.com/index.html')
-    cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login(usuario,contrasena)
-    cy.get('a#nameofuser').should('contain.text', usuario)
-    cy.wait(10000)
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    const usuario = "random01";
+    const contrasena = "random01";
+
+    Logger.stepNumber(1);
+    Logger.step("Navigate to Demoblaze page");
+    cy.visit("https://www.demoblaze.com/index.html");
+
+    Logger.stepNumber(2);
+    Logger.step('Click on "Login" link');
+    cy.get('a[data-target="#logInModal"]').click();
+
+    Logger.stepNumber(3);
+    Logger.step(`Login with this credentials: "${usuario}/${contrasena}"`);
+    LoginMethods.login(usuario, contrasena);
+    Logger.verification(`The HomePage should show "Welcome ${usuario}"`);
+    cy.get("a#nameofuser").should("contain.text", usuario);
+  });
+});
